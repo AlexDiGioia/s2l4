ba2ce01adb7e1a337ea43a9278ac1a3b4c177129
AlexDiGioia <digioia.alex2001@gmail.com>
2024-05-09 15:47:06 +0200

    fino es6

--- D4+extra.js
+++ D4+extra.js
@@ -4,6 +4,16 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let result;
+/*function area(l1, l2) {
+  return l1 * l2;
+}*/
+
+area = (l1, l2) => l1 * l2;
+
+result = area(10, 10);
+console.log("Area:", result);
+
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
  La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
@@ -11,6 +21,15 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function crazySum(x, y) {
+  if (x === y) {
+    return (x + y) * 3;
+  } else {
+    return x + y;
+  }
+}
+result = crazySum(2, 2);
+console.log("Crazy Sum:", result);
 
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
@@ -18,6 +37,15 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function crazyDiff(x) {
+  if (x > 19) {
+    return Math.abs((x - 19) * 3);
+  } else {
+    return Math.abs(x - 19);
+  }
+}
+result = crazyDiff(20);
+console.log("Crazy Diff:", result);
 
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
@@ -25,6 +53,15 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+/*function boundary(n) {
+  if ((n >= 20 && n <= 100) || n === 400) return true;
+  return false;
+}*/
+function boundary(n) {
+  return (n >= 20 && n <= 100) || n === 400;  
+}
+result = boundary(400);
+console.log("Boundary:", result);
 
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
@@ -33,6 +70,15 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function epify(string) {
+  if (string.indexOf("EPICODE") === 0) {
+    console.log("sas");
+    return string;
+  }
+  return "EPICODE " + string;
+}
+let stringEs5="Spupazziamoci"
+console.log("Epify:",epify(stringEs5));
 
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
@@ -40,6 +86,25 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function check3and7(n){
+    if(n<0||typeof n!=="number")
+        return -1;
+    if (n%3===0||n%7===0)
+        return 1;
+    return 0;
+}
+let n=12;
+let es6=check3and7(n)
+switch(es6){
+    case -1:
+        console.log("Errore, inserisci un numero positivo");
+        break;
+    case 0:
+        console.log(n,": Numero non divisibile per 3 o per 7");
+        break;
+    case 1:
+        console.log(n,": Numero divisibile per 3 o per 7")
+}
 
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
@@ -66,10 +131,6 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
-
-
-
 
 //---------------------------------------------EXTRA-------------------------------------
 // Esercizi aggiuntivi (facoltativi) per D4
